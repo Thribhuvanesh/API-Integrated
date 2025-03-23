fa9b7f1ecd967bffe8d251c9f29dda7b09efdf5b
Thribhuvanesh <157610604+Thribhuvanesh@users.noreply.github.com>
2025-03-23 19:32:46 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -3,11 +3,11 @@ const searchInput = document.getElementById('search');
 const searchButton = document.getElementById('searchBtn');
 const resultContainer = document.getElementById('result');
 const toggleTheme = document.getElementById('toggleTheme');
-
+const api_key = "accd6d5b940c9be5a18ac2b75e519844"
 // Function to fetch data from API
 async function fetchData(query) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=accd6d5b940c9be5a18ac2b75e519844&units=metric`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${api_key}&units=metric`);
         
         // Check if the response is OK (status 200)
         if (!response.ok) {
